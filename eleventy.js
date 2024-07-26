dc52e8360fdd2e9702f41c75553c2f7f86e27ebe
KImberly Black <office@inkboxz.com>
2024-07-26 15:16:54 +0000

    Added 1 file via CloudCannon.

---
+++ eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function(eleventyConfig) {
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      layouts: "_layouts",
+      output: "_site"
+    }
+  };
+};
